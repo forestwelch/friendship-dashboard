0f5dea5758cdc4d810322fb74feb1fd455425315
Nick Welch <nwelchr@gmail.com>
2026-01-01 17:07:42 -0800

    disable console ninja

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    // Prevent Console Ninja extension from injecting code
+    if (!isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        // Block Console Ninja injection
+        "@console-ninja": false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
