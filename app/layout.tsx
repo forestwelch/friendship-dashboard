d5b8c05d5a7d00b78a3c6c108d197089a309e1cd
Nick Welch <nwelchr@gmail.com>
2026-01-11 23:42:16 -0800

    fix font issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,16 @@
 import type { Metadata, Viewport } from "next";
+import { Press_Start_2P } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/lib/theme-context";
 import { QueryProvider } from "@/lib/providers";
 import { WebVitals } from "@/components/WebVitals";
+
+const pressStart2P = Press_Start_2P({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-press-start-2p",
+});
 
 export const metadata: Metadata = {
   title: "Friendship Dashboard",
@@ -22,15 +30,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
-          rel="stylesheet"
-        />
-      </head>
+    <html lang="en" className={pressStart2P.variable}>
       <body>
         <ThemeProvider>
           <QueryProvider>
